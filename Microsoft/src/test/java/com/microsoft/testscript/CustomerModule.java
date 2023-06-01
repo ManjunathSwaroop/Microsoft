32a2a5bad747801ba8308bf668a2e2285207ef1b
ASUS <ASUS@LAPTOP-5KCODA6R>
2023-06-01 11:42:11 +0530

    complete Project maven

---
+++ Microsoft/src/test/java/com/microsoft/testscript/CustomerModule.java
@@ -0,0 +1,12 @@
+package com.microsoft.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+@Test
+public void createCustomer() {
+	Reporter.log("Create customer", true);
+	
+}
+}
